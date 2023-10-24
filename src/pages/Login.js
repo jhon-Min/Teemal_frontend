a6fb03642040bb86aa860c719c4d7839cd96a086
jhon-Min <minnyisay@gmail.com>
2023-10-24 15:53:43 +0630

    develop login feature

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,12 +1,85 @@
-import { Row, Card, Divider, Col, Button, Layout, Form, Input } from "antd";
+import {
+  Row,
+  Card,
+  Divider,
+  Col,
+  Button,
+  Layout,
+  Form,
+  Input,
+  message,
+} from "antd";
+import { login } from "../services/api/loginApi";
 
 export function Login() {
+  function loginHandler(value) {
+    console.log(value);
+    login("login", value)
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  }
+
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+    message.error(errorInfo);
+  };
   return (
     <>
       <Row justify="center" align="middle" style={{ height: "100vh" }}>
         <Col xs={24} sm={24} md={12} lg={8}>
           <Card>
-            <h1 style={{ marginBottom: "25px" }}>Bookmal</h1>
+            <h1 style={{ marginBottom: "25px", textAlign: "center" }}>
+              Teemal
+            </h1>
+
+            <div>
+              <Form
+                layout="vertical"
+                name="basic"
+                initialValues={{
+                  remember: true,
+                }}
+                onFinish={loginHandler}
+                onFinishFailed={onFinishFailed}
+                autoComplete="off"
+              >
+                <Form.Item
+                  label="Username"
+                  name="loginName"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Please input your username!",
+                    },
+                  ]}
+                >
+                  <Input />
+                </Form.Item>
+
+                <Form.Item
+                  label="Password"
+                  name="password"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Please input your password!",
+                    },
+                  ]}
+                >
+                  <Input.Password />
+                </Form.Item>
+
+                <Form.Item>
+                  <Button
+                    type="primary"
+                    htmlType="submit"
+                    style={{ width: "100%", marginTop: "20px" }}
+                  >
+                    Login
+                  </Button>
+                </Form.Item>
+              </Form>
+            </div>
           </Card>
         </Col>
       </Row>
--- src/services/api/loginApi.js
+++ src/services/api/loginApi.js
@@ -2,5 +2,6 @@ import axios from "axios";
 import { endpoint } from "../config";
 
 export function login(url, body) {
+  console.log(endpoint);
   return axios.post(`${endpoint}${url}`, body);
 }
